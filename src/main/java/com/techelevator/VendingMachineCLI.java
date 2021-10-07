e4f5d282da259274439db43e9190aff585b09360
Alexandra Bafile <acbafile@gmail.com>
2021-10-07 19:34:03 -0400

    VendingMachine

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,12 +1,58 @@
 package com.techelevator;
 
-// Vending Machine Command Line Interface application
+import java.util.Scanner;
+
 public class VendingMachineCLI {
 
+	double moneyFed;
+	double price;
+
+
 	public VendingMachineCLI(double moneyFed, double price) {
+			this.moneyFed = moneyFed;
+			this.price = price;
+
+
+			Scanner input = new Scanner(System.in);
+			String firstChoiceMade;
+			String secondChoiceMade;
+
+			System.out.println("(1) Display Vending Machine Items");
+			System.out.println("(2) Purchase");
+			System.out.println("(3) Exit");
+			firstChoiceMade = input.nextLine();
+
+			if (firstChoiceMade == "1") {
+
+			} else if (firstChoiceMade == "2") {
+
+			} else if (firstChoiceMade == "3") {
+				System.out.println("");
+			} else {
+				System.out.println("Invalid choice.");
+			}
+
+			System.out.println("(1) Feed Money");
+			System.out.println("(2) Select Product");
+			System.out.println("(3) Finish Purchase");
+			System.out.println();
+			System.out.println("Current money Provided: " + moneyFed);
+			secondChoiceMade = input.nextLine();
+
+			if (secondChoiceMade == "1") {
+
+				System.out.println("How much money (in dollars) would you like to add?");
+				moneyFed = Double.parseDouble(input.nextLine());
+
+			} else if (secondChoiceMade == "2") {
+
+			} else if (secondChoiceMade == "3") {
+
+			} else {
+				System.out.println("Invalid choice.");
+			}
+
+
+		}
 	}
 
-	public static void main(String[] args) {
-		// Make some objects here!
-	}
-}
