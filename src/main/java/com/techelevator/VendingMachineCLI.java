fb6c6cfce109dee09490a686acab88e59521e90f
Alexandra Bafile <acbafile@gmail.com>
2021-10-08 09:56:12 -0400

    capstone

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,21 +1,27 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-public class VendingMachineCLI {
+public class VendingMachineCLI extends Products{
 
 	double moneyFed;
+	String name;
 	double price;
 
 
-	public VendingMachineCLI(double moneyFed, double price) {
-			this.moneyFed = moneyFed;
-			this.price = price;
+	public VendingMachineCLI(double moneyFed, double price, String name) {
 
+			super(name, price);
+			this.moneyFed = moneyFed;
 
 			Scanner input = new Scanner(System.in);
-			String firstChoiceMade;
-			String secondChoiceMade;
+			String firstChoiceMade = "";
+			String secondChoiceMade = "";
 
 			System.out.println("(1) Display Vending Machine Items");
 			System.out.println("(2) Purchase");
@@ -25,19 +31,18 @@ public class VendingMachineCLI {
 			if (firstChoiceMade == "1") {
 
 			} else if (firstChoiceMade == "2") {
-
+				System.out.println("(1) Feed Money");
+				System.out.println("(2) Select Product");
+				System.out.println("(3) Finish Purchase");
+				System.out.println();
+				System.out.println("Current money Provided: " + moneyFed);
+				secondChoiceMade = input.nextLine();
 			} else if (firstChoiceMade == "3") {
-				System.out.println("");
+				System.out.println("Program ending.");
 			} else {
 				System.out.println("Invalid choice.");
 			}
 
-			System.out.println("(1) Feed Money");
-			System.out.println("(2) Select Product");
-			System.out.println("(3) Finish Purchase");
-			System.out.println();
-			System.out.println("Current money Provided: " + moneyFed);
-			secondChoiceMade = input.nextLine();
 
 			if (secondChoiceMade == "1") {
 
@@ -45,7 +50,8 @@ public class VendingMachineCLI {
 				moneyFed = Double.parseDouble(input.nextLine());
 
 			} else if (secondChoiceMade == "2") {
-
+				System.out.println("What product would you like to buy?");
+				name = input.nextLine();
 			} else if (secondChoiceMade == "3") {
 
 			} else {
@@ -53,6 +59,62 @@ public class VendingMachineCLI {
 			}
 
 
+
 		}
+
+	public void printTransaction(String name, String itemSlot, double price) {
+		File transactionLog = new File("transactionLog.txt");
+		if (!transactionLog.exists()) {
+			try {
+				transactionLog.createNewFile();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			try (PrintWriter pw = new PrintWriter(new FileWriter(transactionLog, true))) {
+
+			//	pw.println("DateTime            | Product | Slot | AmountAccepted | Balance");
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		try (PrintWriter pw = new PrintWriter(new FileWriter(transactionLog, true))) {
+
+			LocalDateTime dateTime = LocalDateTime.now();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-uuuu hh:mm a");
+			pw.print(formatter.format(dateTime) + " | ");
+			pw.print(name + " | ");
+			pw.print(itemSlot + "   | ");
+			pw.print(moneyFed + " | ");
+			// pw.println(changeInCoins(moneyFed - itemPrice));
+		} catch (Exception e) {
+			System.out.println("There was a problem writing to the log file.");
+			System.out.println(e.getMessage());
+		}
+
 	}
+
+	@Override
+	public String getSound() {
+		return null;
+	}
+
+}
+/*
+public class Item {
+	private String slot;
+	private String name;
+	private int price;
+	private int stock;
+	private int sales;
+
+	public Item(String slot, String name, int price)
+	{
+		this.slot = slot;
+		this.name = name;
+		this.price = price;
+		stock = 5;
+		sales = 0;
+	}
+ */
 
